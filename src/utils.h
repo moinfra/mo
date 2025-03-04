2ea29eb1d364b74e3930d6e5f272d583c5214bc4
pluveto <zijing.zhang@proton.me>
2025-03-04 13:07:26 +0800

    feat: add MO_WARN macro for warnings

--- src/utils.h
+++ src/utils.h
@@ -4,20 +4,28 @@
 #include <cstdlib>
 #include <cassert>
 
-#define MO_DEBUG(fmt, ...)                                  \
-    do                                                     \
-    {                                                      \
+#define MO_DEBUG(fmt, ...)                              \
+    do                                                  \
+    {                                                   \
         std::printf("[D] %s:%d: ", __FILE__, __LINE__); \
-        std::printf(fmt, ##__VA_ARGS__);                   \
-        std::putchar('\n');                                \
+        std::printf(fmt, ##__VA_ARGS__);                \
+        std::putchar('\n');                             \
     } while (0)
 
-#define MO_ERROR(fmt, ...)                                          \
-    do                                                              \
-    {                                                               \
+#define MO_WARN(fmt, ...)                               \
+    do                                                  \
+    {                                                   \
+        std::printf("[W] %s:%d: ", __FILE__, __LINE__); \
+        std::printf(fmt, ##__VA_ARGS__);                \
+        std::putchar('\n');                             \
+    } while (0)
+
+#define MO_ERROR(fmt, ...)                                       \
+    do                                                           \
+    {                                                            \
         std::fprintf(stderr, "[E] %s:%d: ", __FILE__, __LINE__); \
-        std::fprintf(stderr, fmt, ##__VA_ARGS__);                   \
-        std::fputc('\n', stderr);                                   \
+        std::fprintf(stderr, fmt, ##__VA_ARGS__);                \
+        std::fputc('\n', stderr);                                \
     } while (0)
 
 #ifdef NDEBUG
